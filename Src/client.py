483f2ff0c2af9e03d3e3679aea7c078ca2abbf27
Aaron Kulkarni <aaronkulkarni@utexas.edu>
2023-04-03 19:58:13 -0500

    Client code almost done

--- Src/client.py
+++ Src/client.py
@@ -2,8 +2,15 @@ import socket
 import struct
 import sys
 import threading
+from queue import Queue
+import random
 
 BUFFER_SIZE = 2048
+HELLO = 0
+DATA = 1
+ALIVE = 2
+GOODBYE = 3
+INTERVAL = 2.0
 
 def main():
 
@@ -15,73 +22,129 @@ def main():
     PORT = int(sys.argv[2])
 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    message = pack(0, 1, 1)
+
+
+    global keepGoing #used for intercommunication between socketThread and keyboardThread. when one thread closes, it signals to the other to close as well.
+    keepGoing = True
+    global seqNumber #sequence number for messages
+    seqNumber = 0
+    global sessionID
+    sessionID = random.randint(0, 4294967295)
+    
+    
+    
+    message = pack(HELLO, seqNumber, sessionID)
     s.sendto(message, (HOST, PORT)) #sends hello message
     
-    packet, remote_addr = s.recvfrom(BUFFER_SIZE) #wait for handshake hello message
-    
-    header = unpack(packet[:12])
-    if checkHeader(header) == False:
-        return
 
-    if int(header[2]) == 0: #HELLO message
+    #magic, version, command, seq, sessionID
+
+    while True:
+        timerThread = threading.Timer(INTERVAL, function=closing, args=(HOST, PORT, s))
+        timerThread.start()
+        packet = s.recvfrom(BUFFER_SIZE)[0] #wait for handshake hello message
+        timerThread.cancel()
+        header = unpack(packet[:12])
+        if checkMagicAndVersion(header) == False: #ignore wrong magic and version packets
+            continue
+        if header[2] == GOODBYE:
+            s.close()
+            return
+        print(0)
+        if checkSessionID(header) == False or header[2] == DATA or header[2] == ALIVE:
+            closing(HOST, PORT, s)
+            print(1)
+        break
+
+    global q
+    q = Queue()
+
+    print(2)
+
+    socketThread = threading.Thread(target=handle_socket, args=(HOST, PORT, s))
+    socketThread.start()
+
+    keyboardThread = threading.Thread(target=handle_keyboard, args=(HOST, PORT, s))
+    keyboardThread.start()
+    
+    socketThread.join()
+    keyboardThread.join()
+
+
+def handle_keyboard(HOST, PORT, s):
+    global keepGoing
+    while keepGoing:
+        text = sys.stdin.readline()
+        # Terminates client if input is EOF or 'q'
+        if (not text or (text == "q\n" and sys.stdin.isatty())):
+            q.put("q")
+            keepGoing = False
+            closing(HOST, PORT, s)
+        q.put(text)
+
+    return
+
+
+def handle_socket(HOST, PORT, s):
+    global keepGoing
+    global seqNumber
+
+
+    while keepGoing:
+        #send message
+        seqNumber += 1
+        text = q.get()
+        message = pack(DATA, seqNumber, sessionID)
+        data_message = message + text.encode('utf-8')
+        s.sendto(data_message, (HOST, PORT))
+
         while True:
-            # worker_thread = threading.Thread(target=session, args=(HOST, PORT, s))
-            # worker_thread.start()
-            text = sys.stdin.readline()
-            # Terminates server if input is EOF or 'q'
-            if (not text or (text == "q\n" and sys.stdin.isatty())):
-                break
-            message = pack(1, 1, 1)
-            data_message = message + text.encode('utf-8')
-            s.sendto(data_message, (HOST, PORT))
-            packet, remote_addr = s.recvfrom(BUFFER_SIZE)
+            timerThread = threading.Timer(INTERVAL, function=closing, args=(HOST, PORT, s))
+            timerThread.start()
+            packet = s.recvfrom(BUFFER_SIZE)[0] #wait for handshake hello message
+            timerThread.cancel()
             header = unpack(packet[:12])
-            if checkHeader(header) == False:
-                break
-            if int(header[2]) != 2: #if client receives hello, data, or goodbye
-                break
-        message = pack(3, 1, 1) #goodbye message
-        s.sendto(message, (HOST, PORT))
-        return
+            if checkMagicAndVersion(header) == False: #ignore wrong magic and version packets
+                continue
+            if header[2] == GOODBYE:
+                keepGoing = False
+                closeSocket(s)
+            if checkSessionID or header[2] == DATA or header[2] == HELLO:
+                keepGoing = False
+                closing(HOST, PORT, s)
+            break
         
-        
+    return
 
-def session(HOST, PORT, s):
+def closing(HOST, PORT, s):
+    message = pack(GOODBYE, seqNumber, sessionID)
+    s.sendto(message, (HOST, PORT))
 
-    message = pack(0, 1, 1)
-    s.sendto(message, (HOST, PORT)) #sends hello message
     
-    packet, remote_addr = s.recvfrom(BUFFER_SIZE) #wait for handshake hello message
-    
-    header = unpack(packet[:12])
-    if checkHeader(header) == False:
-        return
+    while True:
+        timerThread = threading.Timer(INTERVAL, function=closeSocket, args=(s))
+        timerThread.start()
+        packet = s.recvfrom(BUFFER_SIZE)[0]
+        timerThread.cancel()
+        header = unpack(packet[:12])
+        if header[2] == GOODBYE:
+            closeSocket(s)
+        else:
+            continue
         
-    if int(header[2]) == 0: #HELLO message
-        while True:
-            text = sys.stdin.readline()
-            # Terminates server if input is EOF or 'q'
-            if (not text or (text == "q\n" and sys.stdin.isatty())):
-                break
-            message = pack(1, 1, 1)
-            data_message = message + text.encode('utf-8')
-            s.sendto(data_message, (HOST, PORT))
-            packet, remote_addr = s.recvfrom(BUFFER_SIZE)
-            header = unpack(packet[:12])
-            if checkHeader(header) == False:
-                break
-            if int(header[2]) != 2: #if client receives hello, data, or goodbye
-                break
-        message = pack(3, 1, 1) #goodbye message
-        s.sendto(message, (HOST, PORT))
-        return     
+def closeSocket(s):
+    s.close()
+    exit(0)
 
+def checkSessionID(header): #checks sessionID
+    if int(header[4] == sessionID):
+        return True
+    return False    
 
-def checkHeader(header):
+def checkMagicAndVersion(header): #checks magic number and version
     if int(header[0]) == 0xC356 and int(header[1]) == 1:
         return True
-    return False       
+    return False    
 
 def pack(command, seq, sessionID):
     return struct.pack('!HBBII', 0xC356, 1, command, seq, sessionID)
@@ -91,3 +154,69 @@ def unpack(data):
 
 if __name__ == "__main__":
     main()
+
+# def session(HOST, PORT, s, q):
+
+#     message = pack(0, 1, 1)
+#     s.sendto(message, (HOST, PORT)) #sends hello message
+    
+#     packet, remote_addr = s.recvfrom(BUFFER_SIZE) #wait for handshake hello message
+    
+#     header = unpack(packet[:12])
+#     if checkHeader(header) == False:
+#         return
+        
+#     if int(header[2]) == 0: #HELLO message
+#         while True:
+#             text = sys.stdin.readline()
+#             # Terminates server if input is EOF or 'q'
+#             if (not text or (text == "q\n" and sys.stdin.isatty())):
+#                 break
+#             message = pack(1, 1, 1)
+#             data_message = message + text.encode('utf-8')
+#             s.sendto(data_message, (HOST, PORT))
+#             packet, remote_addr = s.recvfrom(BUFFER_SIZE)
+#             header = unpack(packet[:12])
+#             if checkHeader(header) == False:
+#                 break
+#             if int(header[2]) != 2: #if client receives hello, data, or goodbye
+#                 break
+#         message = pack(3, 1, 1) #goodbye message
+#         s.sendto(message, (HOST, PORT))
+#         return     
+
+    # s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # message = pack(0, 1, 1)
+    # s.sendto(message, (HOST, PORT)) #sends hello message
+    
+    # packet, remote_addr = s.recvfrom(BUFFER_SIZE) #wait for handshake hello message
+    
+    # header = unpack(packet[:12])
+    # if checkHeader(header) == False:
+    #     return
+
+    # if int(header[2]) == 0: #HELLO message
+    #     while True:
+    #         # worker_thread = threading.Thread(target=session, args=(HOST, PORT, s))
+    #         # worker_thread.start()
+    #         text = sys.stdin.readline()
+    #         # Terminates server if input is EOF or 'q'
+    #         if (not text or (text == "q\n" and sys.stdin.isatty())):
+    #             break
+    #         message = pack(1, 1, 1)
+    #         data_message = message + text.encode('utf-8')
+    #         s.sendto(data_message, (HOST, PORT))
+    #         packet, remote_addr = s.recvfrom(BUFFER_SIZE)
+    #         header = unpack(packet[:12])
+    #         if checkHeader(header) == False:
+    #             break
+    #         if int(header[2]) != 2: #if client receives hello, data, or goodbye
+    #             break
+    #     message = pack(3, 1, 1) #goodbye message
+    #     s.sendto(message, (HOST, PORT))
+    #     return
+
+    #def pack(command, seq, sessionID):
+        #0 HELLO, 1 DATA, 2 ALIVE, 3 GOODBYE
+        # message = pack(1, 1, 1)
+        # data_message = message + text.encode('utf-8')
--- Src/server.py
+++ Src/server.py
@@ -15,44 +15,31 @@ def main():
     HOST = b'0.0.0.0'
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind((HOST, PORT))
-    # sessions = {}
-    # sessionId = 0
-    # if remote_addr not in sessions: #starting a session with a new remote addr
-    #         sessions[remote_addr] = sessionId
-    #         sessionId += 1
 
-    
-    loop = pyuv.Loop.default_loop()
-    serverSocket = pyuv.UPD(loop) # UDP socket read
-    #serverTTY = pyuv.TTY(loop, sys.stdin.fileno(), True) # keyboard read
-    serverSocket.bind(HOST, PORT)
-    serverSocket.start_recv(handle_recv_packet) # start receiving packet from the socket
-    #serverTTY.start_read(handle_keyboard_input) # start listening to keyboard
-    loop.run()
-
-    
     while True:
         packet, remote_addr = s.recvfrom(BUFFER_SIZE)
+        print(packet)
         
         header = unpack(packet[:12])
         if checkHeader(header) == False:
             continue
+
+        sequence = header[3]
+        session = header[4]
         
         if int(header[2]) == 0: #HELLO message
-            message = pack(0, 1, 1)
+            message = pack(0, sequence, session)
             s.sendto(message, remote_addr)
         elif int(header[2]) == 1: #DATA message
             data = packet[12:].decode('utf-8')
             print(data, end='')
-            message = pack(2, 1, 1)
+            message = pack(2, sequence, session)
             s.sendto(message, remote_addr)
         # elif int(header[2]) == 2: #ALIVE message
         #     print("alive")
         elif int(header[2]) == 3: #GOODBYE message
-            message = pack(3, 1, 1)
+            message = pack(3, sequence, session)
             s.sendto(message, remote_addr)
-            serverSocket.stop_recv()
-            loop.stop()
             return
             
 
@@ -66,8 +53,6 @@ def pack(command, seq, sessionID):
 
 def unpack(data):
     return struct.unpack('!HBBII', data)
-
-def handle_recv_packet()
 
 if __name__ == "__main__":
     main()
