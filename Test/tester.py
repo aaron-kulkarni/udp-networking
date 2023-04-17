b7a86e60ed0c54731eccdca4525a5d0596cac02e
devr0306 <devr0306@gmail.com>
2023-04-16 23:47:42 -0500

    Added tests

--- Test/tester.py
+++ Test/tester.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('../Src/')
+import threading
 sys.path.append('../Src/')
 
 
@@ -7,24 +7,39 @@ from server import runServer, endServer, output
 from client import runClient
 
 
-client_args = ['client.py', 'localhost', '1234']
+client_args = ['client.py', 'localhost', '12345']
 def main():
-    runServer(['server.py', '1234'])
+    
+    serverThread = threading.Thread(target=runServer, args=(['server.py', '12345'], ))
+    serverThread.start()
     runTest(test1, ['Session created', 'one', 'two', 'three', 'GOODBYE from client.', 'Session closed'])
+    runTest(test2, ['Session created', 'hello how are', 'you doing?', 'queue this process', 'GOODBYE from client.', 'Session closed'])
+    runTest(test3, ['Session created', 
+                    'Alexey Fyodorovitch Karamazov was the third son of Fyodor Pavlovitch', 
+                    'Karamazov, a land owner well known in our district in his own day, and', 
+                    'still remembered among us owing to his gloomy and tragic death, which',
+                    'happened thirteen years ago, and which I shall describe in its proper',
+                    'place.' 
+                    'GOODBYE from client.', 
+                    'Session closed'])
     endServer()
 
 def runTest(test, expectedOutput):
     test()
-    print(output)
-    # assert output == expectedOutput
+    assert output == expectedOutput
     
 def test1():
-    runClient(parse_file('test1.txt'))
+    file = open('test1.txt')
+    runClient(client_args, file)
     
+def test2():
+    file = open('test2.txt')
+    runClient(client_args, file) 
     
-def parse_file(file):
-    with open(file) as f:
-        return f.readlines()
+def test3():
+    file = open('test3.txt')
+    runClient(client_args, file) 
+
     
 if __name__ == "__main__":
     main()
